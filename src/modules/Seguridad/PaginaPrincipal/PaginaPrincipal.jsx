d9c960b02551043fbfc9d21c45a0f6c1067753f7
JhosepSF <jhosepsanflo@gmail.com>
2024-10-31 09:06:26 -0500

    fotitogod

--- src/modules/Seguridad/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/modules/Seguridad/PaginaPrincipal/PaginaPrincipal.jsx
@@ -97,7 +97,7 @@ export default function PaginaPrincipal() {
           <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
             <img
               alt=""
-              src="https://univerperu.com/wp-content/uploads/2023/07/Universidad-Nacional-de-San-Martin-UNSM.png"
+              src="https://unsm.edu.pe/wp-content/uploads/2022/03/escudologotipo_unsm_2021_lateral_principal_siglas_PNG-370x142.png"
               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             />
           </div>
